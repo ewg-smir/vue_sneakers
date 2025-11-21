3871cf12ed8ca97270c21fc913adcc9145ec7804
ewg-smir <ewg.smir.2002@gmail.com>
2025-11-21 18:57:57 +0300

    ы

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-
+import AppHeader from './components/AppHeader.vue'
 </script>
 
-<script setup></script>
-
 <template>
-<div>d</div>
+  <div class="min-h-screen p-5">
+    <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14">
+      <AppHeader />
+
+      <main class="p-8">
+        <h2>Кроссовки</h2>
+        <div>
+          <div>
+            <img src="/like-1.svg" alt="Like-1" />
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
-
-
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  // Указываем, какие файлы (html, js, vue) Tailwind должен сканировать для поиска классов
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
